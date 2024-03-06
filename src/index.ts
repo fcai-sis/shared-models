e0f5f128fb97117c9f4313c0cafcd7885017e415
Amr Khaled <lewolfje@gmail.com>
2024-03-06 20:17:12 +0200

    remove prerequisite import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from "./models/user.model";
 export * from "./models/department.model";
 export * from "./models/course/course.model";
-export * from "./models/course/prerequisite.model";
 export * from "./models/hall-slot/hall.model";
 export * from "./models/hall-slot/slot.model";
 export * from "./models/instructor-ta/instructor.model";
