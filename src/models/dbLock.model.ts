624877deffc4e4c153aa0cb4b6f16d73901e83e6
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-05-19 20:32:40 +0300

    Remove lock bool

--- src/models/dbLock.model.ts
+++ src/models/dbLock.model.ts
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 export interface IDBLock extends mongoose.Document {
   lockName: string;
-  lock: boolean;
 }
 
 const dbLockSchema = new mongoose.Schema<IDBLock>({
   lockName: { type: String, required: true },
-  lock: { type: Boolean, required: true },
 });
 
 export const dbLockModelName = "DBLock";
