228da56440bd663583b49a4a19a2204fd11ee4b6
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-06-19 15:33:23 +0300

    feat: Update import path for referentialIntegrityValidator in schema.ts

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { referentialIntegrityValidator } from "validators";
+import { referentialIntegrityValidator } from "./validators";
 
 /**
  * A function to create a foreign key schema for a model.
