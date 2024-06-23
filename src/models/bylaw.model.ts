9f86d45a2213288203bf28de5f95fc213ad5b6eb
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-06-23 10:20:58 +0300

    fix exports in bylaw

--- src/models/bylaw.model.ts
+++ src/models/bylaw.model.ts
@@ -3,7 +3,7 @@ import { DepartmentModel } from "./department.model";
 
 // Define the sub-schema for level requirements
 // Interface for grade schema
-interface IGrade {
+export interface IGrade {
   weight: number;
   percentage: {
     min: number;
@@ -12,7 +12,7 @@ interface IGrade {
 }
 
 // Interface for level requirement schema
-interface ILevelRequirement {
+export interface ILevelRequirement {
   mandatoryHours: number;
   electiveHours: number;
   totalHours: number;
@@ -20,7 +20,7 @@ interface ILevelRequirement {
 }
 
 // Interface for graduation project requirement schema
-interface IGraduationProjectRequirement {
+export interface IGraduationProjectRequirement {
   mandatoryHours: number;
   electiveHours: number;
   totalHours: number;
@@ -48,152 +48,162 @@ const gradeWeightValidator = (val: number) => {
 
 const departmentCodeValidator = async (map: Map<string, any>) => {
   const departmentCodes = Array.from(map.keys());
-  const departments = await DepartmentModel.find({ code: { $in: departmentCodes } });
-  const validCodes = departments.map(department => department.code);
-  return departmentCodes.every(code => validCodes.includes(code));
+  const departments = await DepartmentModel.find({
+    code: { $in: departmentCodes },
+  });
+  const validCodes = departments.map((department) => department.code);
+  return departmentCodes.every((code) => validCodes.includes(code));
 };
 
 const numericKeyValidator = (map: Map<string, any>) => {
   const keys = Array.from(map.keys());
-  return keys.every(key => !isNaN(parseInt(key)));
+  return keys.every((key) => !isNaN(parseInt(key)));
 };
 
-const gradeSchema = new mongoose.Schema<IGrade>({
-  weight: {
-    type: Number,
-    validate: [gradeWeightValidator, "Grade weights must be between 0 and 6"],
-    required: true,
-  },
-  percentage: {
-    min: {
+const gradeSchema = new mongoose.Schema<IGrade>(
+  {
+    weight: {
       type: Number,
-      min: [0, "Percentage must be between 0 and 100"],
-      max: [100, "Percentage must be between 0 and 100"],
+      validate: [gradeWeightValidator, "Grade weights must be between 0 and 6"],
       required: true,
     },
-    max: {
-      type: Number,
-      min: [0, "Percentage must be between 0 and 100"],
-      max: [100, "Percentage must be between 0 and 100"],
-      required: true,
+    percentage: {
+      min: {
+        type: Number,
+        min: [0, "Percentage must be between 0 and 100"],
+        max: [100, "Percentage must be between 0 and 100"],
+        required: true,
+      },
+      max: {
+        type: Number,
+        min: [0, "Percentage must be between 0 and 100"],
+        max: [100, "Percentage must be between 0 and 100"],
+        required: true,
+      },
     },
   },
-}, { _id: false });
-
-const levelRequirementSchema = new mongoose.Schema<ILevelRequirement>({
-  mandatoryHours: {
-    type: Number,
-    min: [0, "mandatoryHours must be greater than or equal to 0"],
-    required: function (this: any) {
-      return this.parent().useDetailedHours;
-    }
-  },
-  electiveHours: {
-    type: Number,
-    min: [0, "electiveHours must be greater than or equal to 0"],
-    required: function (this: any) {
-      return this.parent().useDetailedHours;
-    }
-  },
-  totalHours: {
-    type: Number,
-    min: [0, "totalHours must be greater than or equal to 0"],
-    required: function (this: any) {
-      return !this.parent().useDetailedHours;
-    }
-  },
-  maxYears: {
-    type: Number,
-    min: [1, "maxYears must be greater than or equal to 1"],
-    required: true,
-  },
-}, { _id: false }); // _id: false to prevent Mongoose from creating an _id field for each sub-document
-
-// Define the sub-schema for graduation project requirements
-const graduationProjectRequirementSchema = new mongoose.Schema<IGraduationProjectRequirement>({
-  mandatoryHours: {
-    type: Number,
-    min: [0, "mandatoryHours must be greater than or equal to 0"],
-    required: function (this: any) {
-      return this.parent().useDetailedGraduationProjectHours;
-    }
-  },
-  electiveHours: {
-    type: Number,
-    min: [0, "electiveHours must be greater than or equal to 0"],
-    required: function (this: any) {
-      return this.parent().useDetailedGraduationProjectHours;
-    }
-  },
-  totalHours: {
-    type: Number,
-    min: [0, "totalHours must be greater than or equal to 0"],
-    required: function (this: any) {
-      return !this.parent().useDetailedGraduationProjectHours;
-    }
-  },
-}, { _id: false }); // _id: false to prevent Mongoose from creating an _id field for each sub-document
-
-// Main bylaw schema
-export const bylawSchema = new mongoose.Schema<IByLaw>({
-  name: {
-    type: String,
-    required: true,
-  },
-  // gradeWeights is a map of grade to weight { "A": 4, "B": 3, "C": 2, "D": 1, "F": 0} values must be between 0 and 6
-  gradeWeights: {
-    type: Map,
-    of: gradeSchema,
-    required: true
-  },
-  gpaScale: {
-    type: Number,
-    required: true,
-  },
-  // Boolean to determine which hours to use
-  useDetailedHours: {
-    type: Boolean,
-    required: true,
-  },
-  // boolean to determine if detailed hours are used for graduation project
-  useDetailedGraduationProjectHours: {
-    type: Boolean,
-    required: true,
-  },
-  // Map of level to minimum credits required
-  levelRequirements: {
-    type: Map,
-    of: levelRequirementSchema,
-    required: true,
-    validate:
-    {
-      validator: numericKeyValidator,
-      message: "Level requirement keys must be numbers",
-    }
-  },
-  // Graduation project requirements by department
-  graduationProjectRequirements: {
-    type: Map,
-    of: graduationProjectRequirementSchema,
-    required: true,
-    validate:
-    {
-      validator: departmentCodeValidator,
-      message: "Invalid department code in graduationProjectRequirements",
-    }
-  },
-
-  yearApplied: {
-    type: Number,
-    required: true,
-  },
-}, { timestamps: true });
-
-const bylawModelName = "ByLaw";
-
-const BylawModel = mongoose.model<ByLawType>(
-  bylawModelName,
-  bylawSchema
+  { _id: false }
 );
 
-export { BylawModel, bylawModelName };
+const levelRequirementSchema = new mongoose.Schema<ILevelRequirement>(
+  {
+    mandatoryHours: {
+      type: Number,
+      min: [0, "mandatoryHours must be greater than or equal to 0"],
+      required: function (this: any) {
+        return this.parent().useDetailedHours;
+      },
+    },
+    electiveHours: {
+      type: Number,
+      min: [0, "electiveHours must be greater than or equal to 0"],
+      required: function (this: any) {
+        return this.parent().useDetailedHours;
+      },
+    },
+    totalHours: {
+      type: Number,
+      min: [0, "totalHours must be greater than or equal to 0"],
+      required: function (this: any) {
+        return !this.parent().useDetailedHours;
+      },
+    },
+    maxYears: {
+      type: Number,
+      min: [1, "maxYears must be greater than or equal to 1"],
+      required: true,
+    },
+  },
+  { _id: false }
+); // _id: false to prevent Mongoose from creating an _id field for each sub-document
+
+// Define the sub-schema for graduation project requirements
+const graduationProjectRequirementSchema =
+  new mongoose.Schema<IGraduationProjectRequirement>(
+    {
+      mandatoryHours: {
+        type: Number,
+        min: [0, "mandatoryHours must be greater than or equal to 0"],
+        required: function (this: any) {
+          return this.parent().useDetailedGraduationProjectHours;
+        },
+      },
+      electiveHours: {
+        type: Number,
+        min: [0, "electiveHours must be greater than or equal to 0"],
+        required: function (this: any) {
+          return this.parent().useDetailedGraduationProjectHours;
+        },
+      },
+      totalHours: {
+        type: Number,
+        min: [0, "totalHours must be greater than or equal to 0"],
+        required: function (this: any) {
+          return !this.parent().useDetailedGraduationProjectHours;
+        },
+      },
+    },
+    { _id: false }
+  ); // _id: false to prevent Mongoose from creating an _id field for each sub-document
+
+// Main bylaw schema
+export const bylawSchema = new mongoose.Schema<IByLaw>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    // gradeWeights is a map of grade to weight { "A": 4, "B": 3, "C": 2, "D": 1, "F": 0} values must be between 0 and 6
+    gradeWeights: {
+      type: Map,
+      of: gradeSchema,
+      required: true,
+    },
+    gpaScale: {
+      type: Number,
+      required: true,
+    },
+    // Boolean to determine which hours to use
+    useDetailedHours: {
+      type: Boolean,
+      required: true,
+    },
+    // boolean to determine if detailed hours are used for graduation project
+    useDetailedGraduationProjectHours: {
+      type: Boolean,
+      required: true,
+    },
+    // Map of level to minimum credits required
+    levelRequirements: {
+      type: Map,
+      of: levelRequirementSchema,
+      required: true,
+      validate: {
+        validator: numericKeyValidator,
+        message: "Level requirement keys must be numbers",
+      },
+    },
+    // Graduation project requirements by department
+    graduationProjectRequirements: {
+      type: Map,
+      of: graduationProjectRequirementSchema,
+      required: true,
+      validate: {
+        validator: departmentCodeValidator,
+        message: "Invalid department code in graduationProjectRequirements",
+      },
+    },
+
+    yearApplied: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const bylawModelName = "ByLaw";
+
+export const BylawModel =
+  mongoose.models[bylawModelName] ||
+  mongoose.model<ByLawType>(bylawModelName, bylawSchema);
