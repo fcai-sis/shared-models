b5e03b5d1d7f298deafa0cdddc1dc7807343070b
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-06-24 08:16:15 +0300

    feat: Add localized fields for student model

--- src/models/student.model.ts
+++ src/models/student.model.ts
@@ -183,3 +183,80 @@ const studentSchema = new mongoose.Schema<IStudent>({
 export const StudentModel =
   mongoose.models.Student ||
   mongoose.model<IStudent>(studentModelName, studentSchema);
+
+export const studentLocalizedFields: {
+  [key in keyof Omit<StudentType, "user">]: { ar: string; en: string };
+} = {
+  address: {
+    ar: "العنوان",
+    en: "Address",
+  },
+  administration: {
+    ar: "الادارة",
+    en: "Administration",
+  },
+  birthDay: {
+    ar: "يوم الميلاد",
+    en: "Birth Day",
+  },
+  birthMonth: {
+    ar: "شهر الميلاد",
+    en: "Birth Month",
+  },
+  birthPlace: {
+    ar: "محل الميلاد",
+    en: "Birth Place",
+  },
+  birthYear: {
+    ar: "سنة الميلاد",
+    en: "Birth Year",
+  },
+  directorate: {
+    ar: "المديرية",
+    en: "Directorate",
+  },
+  educationType: {
+    ar: "نوع التعليم",
+    en: "Education Type",
+  },
+  fullName: {
+    ar: "الاسم الكامل",
+    en: "Full Name",
+  },
+  gender: {
+    ar: "الجنس",
+    en: "Gender",
+  },
+  governorateId: {
+    ar: "رقم المحافظة",
+    en: "Governorate ID",
+  },
+  nationalId: {
+    ar: "الرقم القومي",
+    en: "National ID",
+  },
+  nationality: {
+    ar: "الجنسية",
+    en: "Nationality",
+  },
+  phoneNumber: {
+    ar: "رقم الهاتف",
+    en: "Phone Number",
+  },
+  religion: {
+    ar: "الديانة",
+    en: "Religion",
+  },
+  scientificDivision: {
+    ar: "الشعبة العلمية",
+    en: "Scientific Division",
+  },
+  studentId: {
+    ar: "رقم الطالب",
+    en: "Student ID",
+  },
+  bylaw: {
+    ar: "اللائحة",
+    en: "Bylaw",
+  },
+};
