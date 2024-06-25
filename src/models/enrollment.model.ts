be8e6105b59af9bfd2ded5b20f0e854db80c5754
Amr Khaled <lewolfje@gmail.com>
2024-06-25 13:20:38 +0300

    hm

--- src/models/enrollment.model.ts
+++ src/models/enrollment.model.ts
@@ -81,14 +81,12 @@ export const enrollmentSchema = new mongoose.Schema<IEnrollment>({
     validate: {
       validator: (v: number) => integerValidator("Group", v),
     },
-    default: null,
   },
   examSeatNumber: {
     type: Number,
     validate: {
       validator: (v: number) => integerValidator("Seat Number", v),
     },
-    default: null,
   },
 
   examHall: foreignKey(hallModelName, false),
@@ -97,14 +95,12 @@ export const enrollmentSchema = new mongoose.Schema<IEnrollment>({
     validate: {
       validator: (v: number) => integerValidator("Final Exam Grade", v),
     },
-    default: null,
   },
   termWorkMark: {
     type: Number,
     validate: {
       validator: (v: number) => integerValidator("Term Work Grade", v),
     },
-    default: null,
   },
   grade: {
     type: String,
