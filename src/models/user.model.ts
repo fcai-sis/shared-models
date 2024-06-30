1900b12fea03155d19281fb9eb7a83913d1bee41
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-06-30 23:42:49 +0300

    chore: Add role field to user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,13 +2,28 @@ import mongoose from "mongoose";
 
 export const userModelName = "User";
 
+export const RoleEnum = [
+  "ADMIN",
+  "STUDENT",
+  "EMPLOYEE",
+  "INSTRUCTOR",
+  "TEACHING_ASSISTANT",
+] as const;
+export type RoleEnumType = (typeof RoleEnum)[number];
+
 export interface IUser extends mongoose.Document {
+  role: RoleEnumType;
   password: string;
 }
 
 export type UserType = Omit<IUser, keyof mongoose.Document>;
 
 const userSchema = new mongoose.Schema<IUser>({
+  role: {
+    type: String,
+    required: true,
+    enum: RoleEnum,
+  },
   password: {
     type: String,
     required: true,
