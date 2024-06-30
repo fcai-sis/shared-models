adc8008b351ae3ffe04f0e9c3d4d35aedf61a42d
Amr Khaled <lewolfje@gmail.com>
2024-06-30 16:10:37 +0300

    add dynamic configuration model

---
+++ src/models/dynamicConfig.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export const dynamicConfigModelName = "DynamicConfiguration";
+
+export interface IDynamicConfig extends mongoose.Document {
+  isCourseEnrollOpen: boolean;
+  isGradProjectRegisterOpen: boolean;
+}
+
+export type DynamicConfigType = Omit<IDynamicConfig, keyof mongoose.Document>;
+
+const dynamicConfigSchema = new mongoose.Schema<IDynamicConfig>({
+  isCourseEnrollOpen: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isGradProjectRegisterOpen: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+export const dynamicConfigModel =
+  mongoose.models[dynamicConfigModelName] ||
+  mongoose.model<IDynamicConfig>(dynamicConfigModelName, dynamicConfigSchema);
